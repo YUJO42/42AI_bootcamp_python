2a72f148aa927073901700f7cff6b4db06530f31
yujo <yujo@student.42seoul.kr>
2020-09-16 14:33:12 +0900

    Day00 ex05 Clear

---
+++ day00/ex05/kata01.py
@@ -0,0 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    kata01.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/09/16 13:27:46 by yujo              #+#    #+#              #
+#    Updated: 2020/09/16 13:51:35 by yujo             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+}
+
+for key in languages.keys() :
+    print(key, "was created by", languages[key])
+
+
+# Expected answer
+
+# Python was created by Guido van Rossum
+# Ruby was created by Yukihiro Matsumoto
+# PHP was created by Rasmus Lerdorf
