e562c60d12c7926b08fafbf813727c60ae507d2e
yujo <yujo@student.42seoul.kr>
2020-09-18 16:26:09 +0900

    day01 init

---
+++ day01/ex00/recipe.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    recipe.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/09/18 15:59:57 by yujo              #+#    #+#              #
+#    Updated: 2020/09/18 16:25:54 by yujo             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class Recipe:
+    def __init__(self):
+        return
+
+    def __str__(self):
+        """Return the string to print with the recipe info"""
+        txt = ""
+        """Your code goes here"""
+        return txt
